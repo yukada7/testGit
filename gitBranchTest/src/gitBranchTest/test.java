0eab4a83b20367f0dc121f97c255e049f0c8414f
yukada7 <mikada7@naver.com>
2023-08-14 23:05:35 +0900

    커밋 다시 시도

--- gitBranchTest/src/gitBranchTest/test.java
+++ gitBranchTest/src/gitBranchTest/test.java
@@ -5,6 +5,8 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("git branch Test입니다.");
+		
+		System.out.println("커밋 되었는지 체크");
 	}
 
 }
