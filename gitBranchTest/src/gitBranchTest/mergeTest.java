511a872f5a1f7e395a444cf89213ff7e48d303ac
yukada7 <mikada7@naver.com>
2023-08-30 23:24:11 +0900

    Test

--- gitBranchTest/src/gitBranchTest/mergeTest.java
+++ gitBranchTest/src/gitBranchTest/mergeTest.java
@@ -4,7 +4,7 @@ public class mergeTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("머지 충돌시 리셋");
+		System.out.println("머지 충돌시 리셋 ");
 	}
 
 }
