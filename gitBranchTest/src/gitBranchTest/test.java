e31d9e994e73fdf8a3aafd5c8240ed573d096ede
yukada7 <mikada7@naver.com>
2024-02-15 01:02:19 +0900

    커밋 날짜 조작 테스트

--- gitBranchTest/src/gitBranchTest/test.java
+++ gitBranchTest/src/gitBranchTest/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("cmd 깃 커밋 테스트");
+		System.out.println("Test in VSCode");
 	}
 
 }
