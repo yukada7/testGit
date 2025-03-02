dd951289d546041287324ee26e72807fb1b4a215
yukada7 <mikada7@naver.com>
2025-03-02 20:18:00 +0900

    First Commit

--- gitBranchTest/src/gitBranchTest/mergeTest.java
+++ gitBranchTest/src/gitBranchTest/mergeTest.java
@@ -7,6 +7,8 @@ public class mergeTest {
 		System.out.println("머지 충돌 시 기존 수정 Delete Test");
 		
 		System.out.println("머지 충돌했을 때, 다른 누군가가 내 머지를 지우면 약간의 수정이라도 충돌이 일어난다.");
+		
+		System.out.println("누군가 이 파일을 1등으로 먼저 수정했습니다.");
 	}
 
 }
