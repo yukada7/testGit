791038f8012a09adb1e13eb1eecc6896cbf8888d
yukada7 <mikada7@naver.com>
2025-03-02 20:25:37 +0900

    Test 머지 충돌 발생

--- gitBranchTest/src/gitBranchTest/mergeTest.java
+++ gitBranchTest/src/gitBranchTest/mergeTest.java
@@ -7,6 +7,8 @@ public class mergeTest {
 		System.out.println("다른 라인을 수정 했을 때 머지 충돌 테스트");
 		
 		System.out.println("머지 충돌했을 때, 다른 누군가가 내 머지를 지우면 약간의 수정이라도 충돌이 일어난다.");
+		
+        System.out.println("누군가 이 파일을 2등으로 수정했습니다.");
 	}
 
 }
