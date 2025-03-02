e2dda40dd7ad61308210cb3cd99f50a421aa7218
yukada7 <mikada7@naver.com>
2025-03-02 20:34:58 +0900

    고의 충돌

--- gitBranchTest/src/gitBranchTest/mergeTest.java
+++ gitBranchTest/src/gitBranchTest/mergeTest.java
@@ -11,6 +11,8 @@ public class mergeTest {
 		System.out.println("누군가 이 파일을 1등으로 먼저 수정했습니다.");
 		
         System.out.println("누군가 이 파일을 2등으로 수정했습니다.");
+        
+        System.out.println("충돌 충돌 테스트");
 	}
 
 }
--- gitBranchTest/src/gitBranchTest/test.java
+++ gitBranchTest/src/gitBranchTest/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("cmd 깃 커밋 테스트");
+		System.out.println("충돌났습니다.");
 		System.out.println("Test in VSCode");
 		System.out.println("소스가 사라짐");
 		System.out.println("누군가 수정했습니다.");
