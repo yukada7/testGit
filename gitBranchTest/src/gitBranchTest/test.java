e0cb9ec0326d59c2530e934514e6a7343c7d9d83
yukada7 <mikada7@naver.com>
2025-03-02 20:38:42 +0900

    오늘 한 작업

--- gitBranchTest/src/gitBranchTest/test.java
+++ gitBranchTest/src/gitBranchTest/test.java
@@ -6,7 +6,7 @@ public class test {
 		// TODO Auto-generated method stub
 		System.out.println("cmd 깃 커밋 테스트");
 		System.out.println("Test in VSCode");
-		System.out.println("소스가 사라짐");
+		System.out.println("퇴근하고 싶어요.");
 	}
 
 }
