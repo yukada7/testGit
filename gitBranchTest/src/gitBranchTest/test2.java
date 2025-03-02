634c1eb057c09415e3e79956164122884fe62181
yukada7 <mikada7@naver.com>
2025-03-02 20:37:44 +0900

    충돌 테스트를 위한 배포

--- gitBranchTest/src/gitBranchTest/test2.java
+++ gitBranchTest/src/gitBranchTest/test2.java
@@ -5,6 +5,7 @@ public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("임의 충돌");
+		System.out.println("다른 수정내용 추가");
 	}
 
 }
--- gitBranchTest/src/gitBranchTest/test3.java
+++ gitBranchTest/src/gitBranchTest/test3.java
@@ -5,6 +5,8 @@ public class test3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("여러 파일 수정했을 때 충돌");
+		
+		System.out.println("다른 수정내용 추가");
 	}
 
 }
